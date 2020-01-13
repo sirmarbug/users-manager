3e812cfbb9c03c474d90e134495f643484a01170
Mariusz Bugajski <mbugajski.dev@gmail.com>
2020-01-13 19:06:35 +0100

    UM-2: Created auth routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,25 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./views/auth/auth.module').then(m => m.AuthModule)
+  },
+  // {
+  //   path: 'dashboard',
+  //   loadChildren: () => import('./views/dashboard/dashboard.module').then(m => m.DashboardModule)
+  // },
+  {
+    path: '',
+    redirectTo: 'auth',
+    pathMatch: 'full'
+  },
+  // {
+  //   path: '**',
+  //   component: NotFoundComponent
+  // }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
