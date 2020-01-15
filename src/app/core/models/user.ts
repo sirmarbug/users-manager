250c9d12f8ebbc98108bfb936c0ce06073d2b33b
Mariusz Bugajski <mbugajski.dev@gmail.com>
2020-01-15 18:21:56 +0100

    UM-9: Valid create new user form

---
+++ src/app/core/models/user.ts
@@ -0,0 +1,14 @@
+import { Role } from './role';
+
+export class User {
+    constructor(
+        public firstName: string = '',
+        public lastName: string = '',
+        public city: string = '',
+        public country: string = '',
+        public mail: string = '',
+        public role: Role = Role.USER,
+        public password: string = '',
+        public repeatPassword: string = '',
+    ) {}
+}
--- src/app/views/dashboard/user-mod/user-mod.component.ts
+++ src/app/views/dashboard/user-mod/user-mod.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { GeneratePasswordPopupComponent } from '@shared/components';
+import { User } from '@core/models';
+import { NGXLogger } from 'ngx-logger';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-user-mod',
@@ -9,15 +12,26 @@ import { GeneratePasswordPopupComponent } from '@shared/components';
 })
 export class UserModComponent implements OnInit {
 
+  user: User;
+
   constructor(
-    private modalService: NgbModal
+    private modalService: NgbModal,
+    private logger: NGXLogger
   ) { }
 
   ngOnInit() {
+    this.user = new User();
   }
 
   onGeneratePasswordClick(): void {
     const modalRef = this.modalService.open(GeneratePasswordPopupComponent);
+  }
+
+  onSubmit(form: NgForm): void {
+    if (form.invalid) {
+      return;
+    }
+    this.logger.debug(this.user);
   }
 
 }
