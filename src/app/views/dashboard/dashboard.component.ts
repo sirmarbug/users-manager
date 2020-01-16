b8c4f33823324268dabdac36fdbc17c0ef931fd3
Mariusz Bugajski <mbugajski.dev@gmail.com>
2020-01-16 21:17:35 +0100

    UM: Support role

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
+import { UserService } from '@core/services';
+import { User, Role } from '@core/models';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,19 +10,20 @@ import { Router } from '@angular/router';
 })
 export class DashboardComponent implements OnInit {
 
+  currentUser: User;
+
+  get name(): string {
+    return `${this.currentUser.firstName} ${this.currentUser.lastName}`;
+  }
+
   constructor(
-    private router: Router
+    private router: Router,
+    private userService: UserService,
+    private activatedRoute: ActivatedRoute
   ) { }
 
   ngOnInit() {
-  }
-
-  goToDashboard(): void {
-    this.router.navigateByUrl('dashboard/home');
-  }
-
-  goToUsers(): void {
-    this.router.navigateByUrl('dashboard/users');
+    this.currentUser = this.userService.currentUser;
   }
 
   goToAddUser(): void {
@@ -28,11 +31,25 @@ export class DashboardComponent implements OnInit {
   }
 
   goToUserPreview(): void {
-    this.router.navigateByUrl('dashboard/user-preview');
+    this.router.navigateByUrl(`dashboard/user-preview/${this.currentUser.id}`);
   }
 
   logout(): void {
     this.router.navigateByUrl('auth/login');
+  }
+
+  isAdmin(): boolean {
+    if (this.currentUser.role !== Role.ADMIN) {
+      return false;
+    }
+    return true;
+  }
+
+  isRoute(url: string): boolean {
+    if (this.router.url !== url) {
+      return false;
+    }
+    return true;
   }
 
 }
