d73f6a00494c90712cee5ffbd679d54339dfd7f4
Mariusz Bugajski <mbugajski.dev@gmail.com>
2020-01-16 19:44:36 +0100

    UM-16: Implemented login

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { Observable, from, of } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, map } from 'rxjs/operators';
 import { User } from '@core/models';
+import { NGXLogger } from 'ngx-logger';
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +11,29 @@ import { User } from '@core/models';
 export class UserService {
 
   private userCollection: AngularFirestoreCollection<any>;
+  private session: User = new User();
+
+  get currentUser(): User {
+    return this.session;
+  }
 
   constructor(
-    private db: AngularFirestore
+    private db: AngularFirestore,
+    private logger: NGXLogger,
   ) {
     this.userCollection = this.db.collection('users');
+  }
+
+  login(mail: string, password: string): Observable<boolean> {
+    return this.db.collection('users', ref => ref.where('mail', '==', mail).where('password', '==', password))
+    .valueChanges().pipe(map((_: User[]) => {
+      if (_.length < 1) {
+        return false;
+      }
+      this.logger.debug('Session:', _[0]);
+      this.session = _[0];
+      return true;
+    }));
   }
 
   getUsers(): Observable<any> {
--- src/app/views/auth/login/login.component.ts
+++ src/app/views/auth/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
 import { NGXLogger } from 'ngx-logger';
+import { UserService } from '@core/services';
+import { User } from 'firebase';
 
 @Component({
   selector: 'app-login',
@@ -12,10 +14,12 @@ export class LoginComponent implements OnInit {
 
   mail = '';
   password = '';
+  error = false;
 
   constructor(
     private router: Router,
-    private logger: NGXLogger
+    private logger: NGXLogger,
+    private userService: UserService
   ) { }
 
   ngOnInit() {
@@ -25,7 +29,20 @@ export class LoginComponent implements OnInit {
     if (form.invalid) {
       return;
     }
-    this.router.navigateByUrl('dashboard/home');
+    this.userService.login(this.mail, this.password)
+      .subscribe(
+        (user: boolean) => {
+          if (!user) {
+            this.error = true;
+            return;
+          }
+          this.router.navigateByUrl('dashboard/home');
+        },
+        err => {
+          this.logger.error(err);
+          this.error = true;
+        }
+      );
   }
 
 }
