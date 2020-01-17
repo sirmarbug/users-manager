a8e59745e6cb1cac6c3787ab3907a0f8f7b1f6d8
Mariusz Bugajski <mbugajski.dev@gmail.com>
2020-01-17 18:16:27 +0100

    UM: Created DashboardGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NotFoundComponent } from './views/not-found/not-found.component';
 import { AuthGuard } from '@core/guards/auth.guard';
+import { DashboardGuard } from '@core/guards/dashboard.guard';
 
 
 const routes: Routes = [
@@ -12,6 +13,7 @@ const routes: Routes = [
   },
   {
     path: 'dashboard',
+    canActivate: [DashboardGuard],
     loadChildren: () => import('./views/dashboard/dashboard.module').then(m => m.DashboardModule)
   },
   {
