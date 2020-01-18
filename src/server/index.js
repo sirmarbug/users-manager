57a322d60e2131f1b085b389c1869decdb58891c
Mariusz Bugajski <mbugajski.dev@gmail.com>
2020-01-18 12:20:41 +0100

    UM-34: Created server

--- src/server/index.js
+++ src/server/index.js
@@ -2,29 +2,34 @@ const sgMail = require('@sendgrid/mail');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
 const sendMail = (to, subject, text) => {
-    // sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-    console.log(to);
-    sgMail.setApiKey('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');
-    const msg = {
-        from: 'no-repeat@domain.com',
-        to,
-        subject,
-        text,
-        html: `<strong>${text}</strong>`,
-    };
-    sgMail.send(msg);
+  sgMail.setApiKey('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');
+  const msg = {
+    from: 'no-repeat@domain.com',
+    to,
+    subject,
+    text,
+    html: `<center><h1>USERS MANAGER</h1><h2>Twoje nowe hasło to</h2><strong>${text}</strong><center>`,
+  };
+  sgMail.send(msg);
 }
 
-app.post('/sendMail', (req, res) => {
-    sendMail(req.body.to, req.body.subject, req.body.text);
-    res.send({status: 'ok'});
+app.get('/', (req, res) => {
+  res.send('Api wykorzystywane na potrzeby aplikacji USER MANAGER');
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.post('/sendMail', (req, res) => {
+  console.log(req.body);
+  sendMail(req.body.to, req.body.subject, req.body.text);
+  res.send({
+    status: 'ok'
+  });
+});
+
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
